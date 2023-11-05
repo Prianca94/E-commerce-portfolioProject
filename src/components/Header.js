b6dd2c3c1379b2dd42ec2452e516eab28984c84b
Priyanka <priyalalwani94@gmail.com>
2023-11-05 04:57:48 -0500

    updateheader3

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,6 @@ import React from "react";
 import { Link, NavLink } from "react-router-dom";
 import { BsSearch } from "react-icons/bs";
 import {
-  faBars,
   faCartArrowDown,
   faCodeCompare,
   faHeart,
