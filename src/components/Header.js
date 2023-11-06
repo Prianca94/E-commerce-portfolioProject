a8dd3e59e236e2e24a6771e56dc100452b7d2424
Priyanka <priyalalwani94@gmail.com>
2023-11-06 04:05:09 -0500

    header-responsive

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Link, NavLink } from "react-router-dom";
 import { BsSearch } from "react-icons/bs";
 import {
+  faBars,
   faCartArrowDown,
   faCodeCompare,
   faHeart,
@@ -22,7 +23,7 @@ const Header = () => {
               </p>
             </div>
             <div className="col-6">
-              <p className="text-end text-white">
+              <p className="contactclass text-end text-white">
                 Phone No{" "}
                 <a className="text-white" href="tel:+91 9574382333">
                   {" "}
@@ -33,7 +34,7 @@ const Header = () => {
           </div>
         </div>
       </header>
-      <header className="header-upper py-1">
+      <header className="header-middle-strip py-1">
         <div className="container-xxl">
           <div className="row align-items-center">
             <div className="col-2">
@@ -62,7 +63,7 @@ const Header = () => {
               <div className="header-upper-links d-flex align-items-center justify-content-between">
                 <div>
                   <Link className="d-flex align-items-center gap-10 text-white">
-                    <FontAwesomeIcon className="fs-4" icon={faCodeCompare} />
+                    <FontAwesomeIcon className="fontawesomeclass fs-4" icon={faCodeCompare} />
                     <p className="mb-0">
                       Compare <br /> Products
                     </p>
@@ -70,7 +71,7 @@ const Header = () => {
                 </div>
                 <div>
                   <Link className="d-flex align-items-center gap-10 text-white">
-                    <FontAwesomeIcon className="fs-4" icon={faHeart} />
+                    <FontAwesomeIcon className="fontawesomeclass fs-4" icon={faHeart} />
                     <p className="mb-0">
                       Favourite <br /> WishList
                     </p>
@@ -78,7 +79,7 @@ const Header = () => {
                 </div>
                 <div>
                   <Link className="d-flex align-items-center gap-10 text-white">
-                    <FontAwesomeIcon className="fs-4" icon={faUser} />
+                    <FontAwesomeIcon className="fontawesomeclass fs-4" icon={faUser} />
                     <p className="mb-0">
                       Login <br /> My Account
                     </p>
@@ -93,56 +94,61 @@ const Header = () => {
                     </div>
                   </Link>
                 </div>
+                <div className="menuicon">
+                <FontAwesomeIcon className="fontawesomeclass" icon={faBars} />
+                </div>
               </div>
             </div>
           </div>
         </div>
       </header>
-      <header className="header-bottom-menu p-2">
+      <header className="header-bottom-strip p-2">
         <div className="container-xxl"></div>
         <div className="row">
           <div className="col-12">
-            <div className="menu-bottom d-flex align-items-center gap-30">            
-            <div>
-              <div class="dropdown">
-              <FontAwesomeIcon className="text-white" icon={faSlack}/>
-                <button
-                  class="btn btn-secondary dropdown-toggle bg-transparent border-0 me-5 d-inline-block"
-                  type="button"
-                  id="dropdownMenuButton1"
-                  data-bs-toggle="dropdown"
-                  aria-expanded="false"
-                >
-                  Categories
-                </button>
-                <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
-                  <li>
-                    <Link class="dropdown-item  text-white" href="#">
-                      Action
-                    </Link>
-                  </li>
-                  <li>
-                    <Link class="dropdown-item  text-white" href="#">
-                      Another action
-                    </Link>
-                  </li>
-                  <li>
-                    <Link class="dropdown-item  text-white" href="#">
-                      Something else here
-                    </Link>
-                  </li>
-                </ul>
+            <div className="menu-bottom d-flex align-items-center gap-30">
+              <div>
+                <div class="dropdown">
+                  <FontAwesomeIcon className="text-white" icon={faSlack} />
+                  <button
+                    class="btn btn-secondary dropdown-toggle bg-transparent border-0 me-5 d-inline-block"
+                    type="button"
+                    id="dropdownMenuButton1"
+                    data-bs-toggle="dropdown"
+                    aria-expanded="false"
+                  >
+                    Categories
+                  </button>
+                  <ul
+                    class="dropdown-menu"
+                    aria-labelledby="dropdownMenuButton1">
+                    <li>
+                      <Link class="dropdown-item  text-white" href="#">
+                        Action
+                      </Link>
+                    </li>
+                    <li>
+                      <Link class="dropdown-item  text-white" href="#">
+                        Another action
+                      </Link>
+                    </li>
+                    <li>
+                      <Link class="dropdown-item  text-white" href="#">
+                        Something else here
+                      </Link>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+              <div className="menu-links text-white">
+                <div className="d-flex align-items-center gap-15">
+                  <NavLink className="text-white">Home</NavLink>
+                  <NavLink className="text-white">Our Products</NavLink>
+                  <NavLink className="text-white">Blogs</NavLink>
+                  <NavLink className="text-white">Contact</NavLink>
+                </div>
               </div>
             </div>
-            <div className="menu-links text-white">
-              <div className="d-flex align-items-center gap-15">
-                <NavLink className="text-white">Home</NavLink>
-                <NavLink className="text-white">Our Products</NavLink>
-                <NavLink className="text-white">Blogs</NavLink>
-                <NavLink className="text-white">Contact</NavLink>
-              </div>
-            </div>
-          </div>
           </div>
         </div>
       </header>
